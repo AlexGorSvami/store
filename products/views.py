333fb22c76281bc5a6f095fe6dbc9a8948501361
AlexGorSvami <jagger15696@gmail.com>
2023-08-04 20:53:03 +0300

    created fixtures and filled db

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from products.models import ProductCategory, Product
 
 # Функции = контроллеры = вьюхи
 
@@ -14,26 +14,7 @@ def index(request):
 def products(request):
     content = {
         'title': 'Store - каталог',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': 6090,
-                'descripton': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.'
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'Синяя куртка The North Face',
-                'price': 6090,
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.'
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': 3390,
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.'
-            }
-
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', content)
