3e0f84ae1cfcb50fb30440bbdb0d2b508e1a5939
AlexGorSvami <jagger15696@gmail.com>
2023-08-08 20:05:15 +0300

    created registration

--- users/views.py
+++ users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-# Create your views here.
+from users.models import User
+from users.forms import UserLoginFrom, UserRegistrationForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginFrom(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+
+    else:
+        form = UserLoginFrom()
+    context = {'form': UserLoginFrom()}
+    return render(request, 'users/login.html', context)
+
+
+def registation(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'users/registration.html', context)
