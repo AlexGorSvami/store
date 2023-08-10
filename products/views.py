8abdfabbe1d4491ac2e2c64fab508c434a7dddb3
AlexGorSvami <jagger15696@gmail.com>
2023-08-10 22:11:19 +0300

    created basket and implemented methods add and delete

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Basket
+from users.models import User
+
 
 # Функции = контроллеры = вьюхи
 
@@ -18,3 +20,23 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', content)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
