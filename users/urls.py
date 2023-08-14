6634ac393f5fba88a9c891f8ab467c9d1d71fb55
AlexGorSvami <jagger15696@gmail.com>
2023-08-14 20:08:09 +0300

    converted functions registration and profile to class UserRegistrationView and UserProfileView

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
-from users.views import login, registation, profile, logout
+from users.views import login, UserRegistrationView, UserProfileView, logout
+
+from django.contrib.auth.decorators import login_required
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registation, name='registration'),
-    path('profile/', profile, name='profile'),
-    path('logout/', logout, name='logout')
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
+    path('logout/', logout, name='logout'),
 
 ]
