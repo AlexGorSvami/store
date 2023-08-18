4628cd37f3ee88fbedbebcdd46141c8ebc536102
AlexGorSvami <jagger15696@gmail.com>
2023-08-18 19:53:20 +0300

    install redis and cached part of the code

--- store/settings.py
+++ store/settings.py
@@ -86,7 +86,18 @@ WSGI_APPLICATION = 'store.wsgi.application'
 INTERNAL_IPS = [
     '127.0.0.1',
     'localhost',
+
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
