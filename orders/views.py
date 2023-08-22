6f106acde1ac0be994624dd6c3311989d06837ff
AlexGorSvami <jagger15696@gmail.com>
2023-08-22 15:57:04 +0300

    Connected payment from store

--- orders/views.py
+++ orders/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponse
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -31,14 +32,9 @@ class OrderCreateView(TitleMixin, CreateView):
 
     def post(self, request, *args, **kwargs):
         super(OrderCreateView, self).post(request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    # Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                    'price': 'price_1NhXtkKVZ19satgWG4fP7uIF',
-                    'quantity': 1,
-                },
-            ],
+            line_items=baskets.stripe_products,
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order-success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order-canceled')),
@@ -68,15 +64,14 @@ def stripe_webhook_view(request):
         return HttpResponse(status=400)
 
     # Handle the event
-    if event.type == 'payment_intent.succeeded':
-        payment_intent = event.data.object  # contains a stripe.PaymentIntent
-        print('PaymentIntent was successful!')
-    elif event.type == 'payment_method.attached':
-        payment_method = event.data.object  # contains a stripe.PaymentMethod
-        print('PaymentMethod was attached to a Customer!')
-    # ... handle other event types
-    else:
-        print('Unhandled event type {}'.format(event.type))
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        fulfill_order(session)
 
     return HttpResponse(status=200)
 
+
+def fulfill_order(session):
+    order_in = int(session.metadata.order_id)
+    print('Fulfilling order')
